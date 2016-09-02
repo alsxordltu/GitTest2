0721dbd696a3d3a0ced936663b2851bac4e261bf
MT-717-8 <MT-717-8@MT-717-8-PC>
2016-09-02 16:06:06 +0900

    dfdf

--- GitTest2/src/com/example/GitTest.java
+++ GitTest2/src/com/example/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
 		System.out.println("내가 팀장이다.wefaewgergatheahethet");
 		System.out.println("안물");
 		System.out.println("노예");
-		System.out.println("오오오오오오ㅗ오옹오옹옹!!!!!!!!!!!!!!!!");
+		System.out.println("오오오오오오ㅗsdfdfsdf!!!!!!!!!!!!!!!!");
 		
 		
 	}
