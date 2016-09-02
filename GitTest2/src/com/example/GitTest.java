f1952b661d08fb52820bb7f8e4fa5500b5ac0b54
MT-717-8 <MT-717-8@MT-717-8-PC>
2016-09-02 16:09:17 +0900

    grgwrg

--- GitTest2/src/com/example/GitTest.java
+++ GitTest2/src/com/example/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 		// TODO Auto-generated method stub
 		System.out.println("내가 팀장이다.wefaewgergatheahethet");
 		System.out.println("안물");
-		System.out.println("노예");
+		System.out.println("노예ffgargeg");
 
 		System.out.println("오오오오오오ㅗsdfdfsdf!dfdfsdf!!!!!!!!!!!!!!!");
 		
